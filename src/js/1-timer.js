58aae0b71c579e0b0a302f1a303673e6d29a944f
Roman <vrbox@i.ua>
2024-10-09 11:45:58 +0300

    Init

---
+++ src/js/1-timer.js
@@ -0,0 +1,9 @@
+// Описаний в документації
+//import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+//import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+//const sgallery = new SimpleLightbox('.gallery-item a');
+//sgallery.on('show.simplelightbox');
+
--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -18,10 +18,11 @@ if (formState !== null) {
 inputForm.addEventListener("input", event => {
     if (event.target.name === "email") {
         formData.email = event.target.value;
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
     } else if (event.target.name === "message") {
         formData.message = event.target.value;
+        localStorage.setItem("feedback-form-state", JSON.stringify(formData));
     }
-    localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 });
 
 const submit = document.querySelector(".feedback-form");
@@ -36,7 +37,9 @@ submit.addEventListener("submit", event => {
         submit.reset(); 
         formData.email = '';
         formData.message = '';
-        localStorage.removeItem("feedback-form-state");
+        inputForm.elements.email.value = '';
+        inputForm.elements.message.value = '';
+        localStorage.clear();
     }
 
 });
