f9442d48a68bff093e08d2a0604a07c99ee802bf
Roman <vrbox@i.ua>
2024-10-15 14:30:11 +0300

    Timer  upgrate

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -27,47 +27,93 @@ const options = {
   },
 };
 
-flatpickr("input#datetime-picker", options);   
+flatpickr("input#datetime-picker", options);
+
+const startButton = document.querySelector("button[data-start]");
+const selectDate = document.querySelector("input#datetime-picker");
 
 function validateSelectDate(date) {
-
   if (date.getTime() < Date.now()) {
-    iziToast.error({ title: 'Error', message: 'Illegal date' });
+    iziToast.error({ title: 'Error', message: 'Please choose a date in the future' });
+    startButton.disabled = true;
   }
   else {
-    iziToast.info({ position: "center", title: 'Hello', message: 'iziToast.info()' });
+    //iziToast.info({ position: "center", title: 'Hello', message: 'iziToast.info()' });
     startButton.disabled = false;
   }
   console.log(date);
 }
 
-const timerDays = document.querySelector(".value[data-days]");
-const timerHours = document.querySelector(".value[data-hours]");
-const timerSeconds = document.querySelector(".value[data-seconds]");
+const timerWidget = {};
+timerWidget.days = document.querySelector(".value[data-days]");
+timerWidget.hours = document.querySelector(".value[data-hours]");
+timerWidget.minutes = document.querySelector(".value[data-minutes]");
+timerWidget.seconds = document.querySelector(".value[data-seconds]");
 
-const startButton = document.querySelector("button[data-start]");
 startButton.disabled = true;
 startButton.addEventListener('click', () => {
-  
   if (deadLine.getTime() > Date.now()) {
-    let counter = 19;
-    timerSeconds.textContent = counter;
+    selectDate.disabled = true;
     startButton.disabled = true;
-
-    const timeinterval = setInterval(() => {
-      if (counter <= 0) {
-        clearInterval(timeinterval);
-        iziToast.info({ position: "center", title: 'Hello', message: 'iziToast.info()' });
+    let counter = deadLine.getTime() - Date.now();
+    const delay = counter % 1000;
+    counter -= delay;
+    updateTimerValues(timerWidget, counter);
+    setTimeout(() => {
+      if (counter > 0) {
+        const timeinterval = setInterval(() => {
+          if ((counter - 1000) == 0) {
+            clearInterval(timeinterval);
+            iziToast.info({ position: "center", title: 'Finished', message: 'Event happened' });
+            updateTimerValues(timerWidget, 0);
+            selectDate.disabled = false;
+          } else {
+            counter -= 1000;
+            updateTimerValues(timerWidget, counter);
+          }
+        }, 1000);
       } else {
-        counter--;
-        timerSeconds.textContent = counter;
+        iziToast.info({ position: "center", title: 'Finished', message: 'Event happened' });
+        startButton.disabled = false;
       }
-    }, 1000);
+    }, delay);
   } else {
-    iziToast.info({ position: "center", title: 'Hello', message: 'iziToast.info()' });
+    iziToast.info({ position: "center", title: 'Finished', message: 'Event happened' });
+    startButton.disabled = false;
   }
-  
 });
 
+function updateTimerValues(timerWidget, counter)
+{
+    const { days, hours, minutes, seconds } = timerWidget;
+    seconds.textContent = addLeadingZero(convertMs(counter).seconds);
+    minutes.textContent = addLeadingZero(convertMs(counter).minutes);
+    hours.textContent = addLeadingZero(convertMs(counter).hours);
+    days.textContent = addLeadingZero(convertMs(counter).days);
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  if (value > 9) return value;
+  return value.toString().padStart(2, '0');
+}
 
 
